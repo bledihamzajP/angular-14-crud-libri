33fb5769d7cbb69026a1f7d697f5bdb905c3c8d9
bledihamzajP <bledi.hamzaj@mat.tn.it>
2023-03-03 15:23:27 +0100

    prova

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddLibriComponent } from './components/add-libri/add-libri.component';
+import { LibriInfoComponent } from './components/libri-info/libri-info.component';
+import { LibriListaComponent } from './components/libri-lista/libri-lista.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddLibriComponent,
+    LibriInfoComponent,
+    LibriListaComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
